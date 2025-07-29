4baef8719941e30b612fa25ae1c084a723f6a307
Swapnil Gandhale <swapnilgandhale08@gmail.com>
2025-07-29 11:18:50 +0530

    initialized

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 export default function Navbar() {
   return (
-    <nav className="card" style={{position: 'sticky', top: 0, background: '#fff'}}>
+    <nav className="navbar" >
       <a href="#home">Home</a> | <a href="#about">About</a> | <a href="#experience">Experience</a> | <a href="#education">Education</a> | <a href="#projects">Projects</a> | <a href="#contact">Contact</a>
     </nav>
   )
