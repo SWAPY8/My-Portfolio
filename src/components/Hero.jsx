15bbdc4c64af9190e3f8a65815b0710bd87a06e2
Swapnil Gandhale <swapnilgandhale08@gmail.com>
2025-07-30 17:18:51 +0530

    done with home

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,23 @@
-import styles from './Hero.module.css';
+import styles from "./Hero.module.css";
 export default function Hero() {
   return (
-    <section id="home" className="card" style={{textAlign: 'center'}}>
-      <h1>I'm Swapnil Gandhale</h1>
-      <h3>A Tech Enthusiast & Web Developer (Freelancer)</h3>
-      <p>Pune, India | Experience: 1 Year</p>
-      <a href="/cv.pdf" download>
-        <button className="download">Download CV</button>
-      </a>
-      <div style={{marginTop: '10px'}}>
-        <a href="https://github.com/yourusername">GitHub</a> | 
-        <a href="https://linkedin.com/in/yourusername">LinkedIn</a>
+    <section id="home" className="hero">
+      <div className={styles.photo}>
+        <img
+          src="../src/assets/profile_picture.jpg"
+          alt="swapy-img"
+          width="250px"
+        />
+      </div>
+      <div className={styles.info}>
+        <h1>I'm Swapnil Gandhale</h1>
+        <h3>Full-Stack Developer & A Tech Enthusiast</h3>
+        <p>Pune, India </p>
+        <div className={styles.btns}>
+          <button className={styles.github}>GitHub</button>
+          <button className={styles.resume}>Resume</button>
+        </div>
       </div>
     </section>
-  )
+  );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,12 @@
 import styles from './Navbar.module.css';
 export default function Navbar() {
   return (
-    <nav className="navbar" >
-      <a href="#home">Home</a> <a href="#education">Education</a>  <a href="#projects">Projects</a>  <a href="#contact">Contact</a>
+    <nav className={styles.navbar} >
+      <a href="#home" className={styles.link}>Home</a> 
+      <a href="#experience" className={styles.link}>Experience</a> 
+      <a href="#education" className={styles.link}>Education</a>  
+      <a href="#projects"className={styles.link}>Projects</a>  
+      <a href="#contact" className={styles.link}>Contact</a>
     </nav>
   )
 }
