abcc893b0058cc34b699d96638a762e7fa37888c
SWAPY8 <swapnilofficial08@gmail.com>
2025-09-08 19:47:16 +0530

    done with education

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -8,34 +8,63 @@ export default function Education() {
           Bachelor of Engineering in Artificial Intelligence & Data Science
         </h3>
         <p>Savitribai Phule Pune University — 2022 – 2026</p>
-        <p>Current CGPA: 8.8</p>
+        <p className={styles.gpastyle}>Current CGPA : 8.8</p>
         <p>
-          Specialized in Artificial Intelligence, Data Science, and Full-Stack
+          Specialized in Artificial Intelligence, Data Science and Full-Stack
           Web Development.
         </p>
         <p>
           Key Courses: Data Structures & Algorithms, Machine Learning, Database
-          Management, Software Engineering, Cloud Computing, UI/UX.
+          Management, <br /> Software Engineering, Cloud Computing, UI/UX.
         </p>
-        <p>Academic Projects:
+        <p>
+          Academic Projects:
           <ul>
-            <li>Student Management System (Spring Boot + MySQL) – CRUD operations,
-          Excel export.</li>
-            <li>NavEye (Computer Vision + LiDAR) – Object detection for navigation
-          assistance.</li>
-            <li>DDoS Simulation (Python + Flask) – Simulated network attacks for
-          research.</li>
+            <li>
+              Student Management System (Spring Boot + MySQL) – CRUD operations,
+              Excel export.
+            </li>
+            <li>
+              NavEye (Computer Vision + LiDAR) – Object detection for navigation
+              assistance.
+            </li>
+            <li>
+              DDoS Simulation (Python + Flask) – Simulated network attacks for
+              research.
+            </li>
           </ul>
-          
         </p>
+        <button className={styles.btnstyle}>Result</button>
+      </div>
+
+      <div className={styles.educard}>
+        <h3>Higher Secondary (HSC) – Science </h3>
+        <p>L.G.Bansude Junior College palasdeo, Indapur | 2021 – 2022</p>
+        <p className={styles.gpastyle}>Grade : 78%</p>
+        <p>
+          <ul>
+            <li>Stream: Science (Physics, Chemistry, Mathematics, English)</li>
+            <li>
+              Actively participated in science exhibitions and inter-college
+              quiz competitions
+            </li>
+          </ul>
+        </p>
+        <button className={styles.btnstyle}>Result</button>
       </div>
       <div className={styles.educard}>
-        <h3>High School — L. G. Ansude Jr. College (2018–2020)</h3>
-        <p>Grade: First Class Distinction</p>
-      </div>
-      <div className={styles.educard}>
-        <h3>High School — L. G. Ansude Jr. College (2018–2020)</h3>
-        <p>Grade: First Class Distinction</p>
+        <h3>Secondary School (SSC)</h3>
+        <p>L.G.Bansude School palasdeo, Indapur | 2019 – 2020</p>
+        <p className={styles.gpastyle}>Grade : 89.60%</p>
+        <p>
+          <ul>
+            <li>Excelled in core subjects: Mathematics, Science and English</li>
+            <li>
+              Represented class in school-level cricket tournaments.
+            </li>
+          </ul>
+        </p>
+        <button className={styles.btnstyle}>Result</button>
       </div>
     </section>
   );
